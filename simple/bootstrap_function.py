20b69f1a54f38d86cf1efca9e319f2c6c19cc47f
Jeff van Eek <jeff.van.eek@randstadgroep.nl>
2017-04-26 13:40:06 +0200

    add simple format

---
+++ simple/bootstrap_function.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python2.7
+import json
+import boto3
+import urllib2
+from cfnresponse import send, SUCCESS, FAILED
+import logging
+from optparse import OptionParser
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+class bootstrap(object):
+    reason = None
+    response_data = None
+
+    def __init__(self, event, context):
+        self.event = event
+        self.context = context
+        logger.info("Event: %s" % self.event)
+        logger.info("Context: %s" % self.context)
+        if self.context != None:
+           self.route53 = boto3.session.Session().client('route53')
+        else:
+           self.route53 = boto3.session.Session(profile_name=event['ResourceProperties']['Profile']).client('route53')
+        try:
+            self.vpc_id = event['ResourceProperties']['VpcId']
+            self.hosted_zone_id = event['ResourceProperties']['HostedZoneId']
+            self.region = event['ResourceProperties']['Region']
+        except KeyError as e:
+            self.reason = "Missing required property %s" % e
+            logger.error(self.reason)
+            if self.context:
+                self.send_status(FAILED)
+            return
+
+    def create(self, updating=False):
+        try:
+            response = self.route53.associate_vpc_with_hosted_zone(
+                HostedZoneId=self.hosted_zone_id,
+                VPC={
+                    'VPCRegion': self.region,
+                    'VPCId': self.vpc_id
+                }
+            )
+            logger.info("Response: %s" % response)
+            if not updating:
+                self.send_status(SUCCESS)
+        except Exception as e:
+            self.reason = "Create Vpc Hosted Zone association call Failed %s" % e
+            logger.error(self.reason)
+            if self.context:
+                self.send_status(FAILED)
+            return
+
+    def delete(self, updating=False):
+        if updating:
+            hosted_zone = self.event['OldResourceProperties']['HostedZoneId']
+            vpc_id = self.event['OldResourceProperties']['VpcId']
+            region = self.event['OldResourceProperties']['Region']
+            logger.info("Update dissociate: %s from %s" % (vpc_id,hosted_zone))
+        else:
+            hosted_zone = self.hosted_zone_id
+            vpc_id = self.vpc_id
+            region = self.region
+            logger.info("Dissociate: %s from %s" % (vpc_id,hosted_zone))
+        try:
+            self.route53.disassociate_vpc_from_hosted_zone(
+                HostedZoneId=hosted_zone,
+                VPC={
+                    'VPCRegion': region,
+                    'VPCId': vpc_id
+                }
+            )
+            if not updating:
+                self.send_status(SUCCESS)
+        except Exception as e:
+            self.reason = "Delete Vpc Hosted Zone association call Failed %s" % e
+            logger.error(self.reason)
+            if self.context:
+                self.send_status(FAILED)
+            return
+
+    def update(self):
+        self.create(updating=True)
+        self.delete(updating=True)
+        self.send_status(SUCCESS)
+
+    def send_status(self, PASS_OR_FAIL):
+        send(
+            self.event,
+            self.context,
+            PASS_OR_FAIL,
+            reason=self.reason,
+            response_data=self.response_data
+        )
+
+def lambda_handler(event, context):
+    website = bootstrap(event, context)
+    if event['RequestType'] == 'Delete':
+        website.delete()
+        return
+    if event['RequestType'] == 'Create':
+        website.create()
+        return
+    if event['RequestType'] == 'Update':
+        website.update()
+        return
+    logger.info("Received event: " + json.dumps(event, indent=2))
+    if context:
+        send(event, context, FAILED, reason="Unknown Request Type %s" % event['RequestType'])
+
+if __name__ == "__main__":
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-r","--region", help="Region in which to run.")
+    parser.add_option("-a","--api", help="API endpoint.")
+    parser.add_option("-s","--hosted_website", help="S3 bucket hosting website.")
+    parser.add_option("-p","--profile", help="Profile name to use when connecting to aws.", default="default")
+    parser.add_option("-x","--execute", help="Execute an update create or delete.", default="create")
+    (opts, args) = parser.parse_args()
+
+    options_broken = False
+    if not opts.api:
+        logger.error("Must Specify API endpoint")
+        options_broken = True
+    if not opts.hosted_website:
+        logger.error("Must Specify S3 Bucket hosting website")
+        options_broken = True
+    if options_broken:
+        parser.print_help()
+        exit(1)
+    if opts.execute != 'Update':
+        event = { 'RequestType': opts.execute, 'ResourceProperties': { 'HostedZoneId': opts.hosted_zone_id, 'VpcId': opts.vpc_id, 'Profile': opts.profile, 'Region': opts.region } }
+    else:
+        event = { 'RequestType': opts.execute, 'ResourceProperties': { 'HostedZoneId': opts.hosted_zone_id, 'VpcId': opts.vpc_id, 'Profile': opts.profile, 'Region': opts.region }, 'OldResourceProperties': { 'HostedZoneId': opts.old_hosted_zone_id, 'VpcId': opts.old_vpc_id, 'Profile': opts.profile, 'Region': opts.region } }
+    lambda_handler(event, None)
