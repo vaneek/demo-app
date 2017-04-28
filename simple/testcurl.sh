be2ce8baac5fc568245afbfa232961c567bcc563
vaneek <vaneek@users.noreply.github.com>
2017-04-28 21:08:41 +0200

    Delete testcurl.sh

--- simple/testcurl.sh
+++
@@ -1,2 +0,0 @@
-curl -X PUT -d 'lots and lots of text' https://lo6cwyrqs5.execute-api.eu-west-1.amazonaws.com/Prod/resource/5
-curl -X GET  https://lo6cwyrqs5.execute-api.eu-west-1.amazonaws.com/Prod/resource/5
